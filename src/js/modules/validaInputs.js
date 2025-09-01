350af3456f9a3c6794c4b92a7413504e759cc6b9
jose33-afk <josedev337@gmail.com>
2025-09-01 18:34:31 -0300

    Sistema de validacao simples
    sistema para evitar que repita um caracter seja numero ou letra mais de 5 vezes.

--- src/js/modules/validaInputs.js
+++ src/js/modules/validaInputs.js
@@ -1,5 +1,21 @@
 import { cepInput, NumCll } from "../components/inputTrocaPlace.js";
+import { pegaElemento } from "./ultis.js";
+const listadivs = pegaElemento("div", 2);
 
+//validar spans.
+const validarInput = (nome, valor) => {
+  if (/(.)\1{5,}/.test(valor)) alert(`Tem certeza que isso e um(a) ${nome}`);
+};
+
+for (let div of listadivs) {
+  let nome = div.querySelector("label").textContent;
+  let input = div.querySelector("input");
+  input.addEventListener("input", () => {
+    validarInput(nome, input.value);
+  });
+}
+
+//Validar numeros.
 const SomenteNumeros = (elemento, nome, comprimento) => {
   elemento.addEventListener("input", () => {
     if (elemento.value.length === comprimento) {
@@ -14,6 +30,6 @@ const SomenteNumeros = (elemento, nome, comprimento) => {
   });
 };
 
-SomenteNumeros(cepInput, 'Cep', 8);
-SomenteNumeros(NumCll, 'Numero', 11);
+SomenteNumeros(cepInput, "Cep", 8);
+SomenteNumeros(NumCll, "Numero", 11);
 //teste e pra testar expressoes regulares.
